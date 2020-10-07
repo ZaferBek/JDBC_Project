10be83833eb63fd2f058e4db22c6339a32896499
ZaferBek <zaferbek22@gmail.com>
2020-10-07 21:30:02 +0100

    Change2

--- src/test/java/add_file_here/AvengerInfinity.java
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -4,6 +4,10 @@ public class AvengerInfinity {
     public static void main(String[] args) {
         System.out.println("Hi to Avanger fans");
 
+        System.out.println("Change 2 is coming");
+        //more are coming
+
+
     }
 
 }
